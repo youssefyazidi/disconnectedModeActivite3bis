12c04f31e10b09ec16ec97d321388d9739e64fb5
yazidi <yazidi17@gmail.com>
2022-12-26 21:00:07 +0100

    Version1

---
+++ disconnectedModeActivite3bis/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace disconnectedModeActivite3bis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdPass = new System.Windows.Forms.DataGridView();
+            this.grdPersPass = new System.Windows.Forms.DataGridView();
+            this.grdPers = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdPass
+            // 
+            this.grdPass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPass.Location = new System.Drawing.Point(624, 74);
+            this.grdPass.Name = "grdPass";
+            this.grdPass.Size = new System.Drawing.Size(187, 300);
+            this.grdPass.TabIndex = 5;
+            this.grdPass.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdPass_CellContentClick);
+            this.grdPass.CurrentCellChanged += new System.EventHandler(this.grdPass_CurrentCellChanged);
+            // 
+            // grdPersPass
+            // 
+            this.grdPersPass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPersPass.Location = new System.Drawing.Point(342, 74);
+            this.grdPersPass.Name = "grdPersPass";
+            this.grdPersPass.Size = new System.Drawing.Size(202, 300);
+            this.grdPersPass.TabIndex = 4;
+            // 
+            // grdPers
+            // 
+            this.grdPers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdPers.Location = new System.Drawing.Point(67, 74);
+            this.grdPers.Name = "grdPers";
+            this.grdPers.Size = new System.Drawing.Size(216, 300);
+            this.grdPers.TabIndex = 3;
+            this.grdPers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdPers_CellContentClick);
+            this.grdPers.CurrentCellChanged += new System.EventHandler(this.grdPers_CurrentCellChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 448);
+            this.Controls.Add(this.grdPass);
+            this.Controls.Add(this.grdPersPass);
+            this.Controls.Add(this.grdPers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPersPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grdPers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdPass;
+        private System.Windows.Forms.DataGridView grdPersPass;
+        private System.Windows.Forms.DataGridView grdPers;
+    }
+}
+
---
+++ disconnectedModeActivite3bis/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace disconnectedModeActivite3bis
+{
+    public partial class Form1 : Form
+    {
+        //Represente une base locale
+        private DataSet MonData = new DataSet("MonData");
+        private bool load = false;
+
+        //La chaine de connexion
+        private string connString = 
+         @"Data Source=.\SQLEXPRESS;Initial Catalog=passionsDB;Integrated Security=true";
+        //On va avoir besoin de la connexion
+        private SqlConnection con;
+        //Le pont = Créer partiellemnt le schema et le Data
+        //à partir d'une base de données distante
+        private SqlDataAdapter adapterPersonnes;
+        private SqlDataAdapter adapterPassions;
+        private SqlDataAdapter adapterPersPass;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection(connString);
+            //Creer un objet Commande
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM Personnes";
+            //le cmd represente le select
+            adapterPersonnes = new SqlDataAdapter(cmd);
+
+            cmd = new SqlCommand("SELECT * FROM Passions",con);
+            adapterPassions = new SqlDataAdapter(cmd);
+
+            cmd = new SqlCommand("SELECT * FROM PersPass", con);
+            adapterPersPass = new SqlDataAdapter(cmd);
+
+            //Remplir le DataSet
+            //l'adapter ramene seulement la structure et le data (pas de contrainte)
+            adapterPersonnes.Fill(MonData, "Personnes");
+            adapterPassions.Fill(MonData, "Passions");
+            adapterPersPass.Fill(MonData, "PersPass");
+
+            //Les liaisons 
+            grdPers.DataSource = MonData;
+            grdPers.DataMember = "Personnes";
+
+            grdPass.DataSource = MonData;
+            grdPass.DataMember = "Passions";
+
+            grdPersPass.DataSource = MonData;
+            grdPersPass.DataMember = "PersPass";
+
+
+            MonData.Tables["Personnes"].PrimaryKey = new DataColumn[] {
+               MonData.Tables["Personnes"].Columns["IndexNom"]
+           };
+
+            MonData.Tables["Passions"].PrimaryKey = new DataColumn[] {
+               MonData.Tables["Passions"].Columns["IndexPass"]
+           };
+            MonData.Tables["PersPass"].PrimaryKey = new DataColumn[] {
+               MonData.Tables["PersPass"].Columns["IndexNom"],
+                 MonData.Tables["PersPass"].Columns["IndexPass"]
+           };
+
+            //Declaration des relations 
+
+            DataRelation rel =
+                new DataRelation(
+                    "PersInt",
+                    MonData.Tables["Personnes"].Columns["IndexNom"],
+                    MonData.Tables["PersPass"].Columns["IndexNom"]
+                );
+
+            MonData.Relations.Add(rel);
+
+            rel =
+                new DataRelation(
+                    "PassInt",
+                    MonData.Tables["Passions"].Columns["IndexPass"],
+                    MonData.Tables["PersPass"].Columns["IndexPass"]
+                );
+
+            MonData.Relations.Add(rel);
+
+            load = true;
+
+        }
+
+        private void grdPers_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void grdPass_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void grdPers_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (load)
+            {
+                DataRow rowPers =
+                     MonData.Tables["Personnes"].Rows[grdPers.CurrentCell.RowIndex];
+                string msg = " Le nom : " + rowPers["Nom"] + "\n";
+                DataRow[] lignesfille = rowPers.GetChildRows("PersInt");
+
+                foreach (DataRow row in lignesfille)
+                {
+                    DataRow parentRow = row.GetParentRow("PassInt");
+                    msg += parentRow["Passion"] + "\n";
+                }
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void grdPass_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (load)
+            {
+                DataRow rowPassion =
+                     MonData.Tables["Passions"].Rows[grdPers.CurrentCell.RowIndex];
+                string msg = " Le nom : " + rowPassion["Passion"] + "\n";
+                DataRow[] lignesfille = rowPassion.GetChildRows("PassInt");
+
+                foreach (DataRow row in lignesfille)
+                {
+                    DataRow parentRow = row.GetParentRow("PersInt");
+                    msg += parentRow["Nom"] + "\n";
+                }
+                MessageBox.Show(msg);
+            }
+        }
+    }
+}
